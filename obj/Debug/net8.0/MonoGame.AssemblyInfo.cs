1156da1b0ee0ce71f846a63a98bf30cf9192c0e6
NicoleSofianidou <niso07001@utb.vaxjo.se>
2025-03-31 09:40:51 +0200

    Update

--- obj/Debug/net8.0/MonoGame.AssemblyInfo.cs
+++ obj/Debug/net8.0/MonoGame.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("MonoGame")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+a823df700910a29253901debade6a784454226bb")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+b754e81b5f1cb4edaa67d0a1681603b4f5262ea1")]
 [assembly: System.Reflection.AssemblyProductAttribute("MonoGame")]
 [assembly: System.Reflection.AssemblyTitleAttribute("MonoGame")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
